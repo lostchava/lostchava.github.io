02a6c8ca49ed6c1bfb0d2da12e8b6c2c30f3951c
andre <andrewsprague80@yahoo.com>
2022-04-25 14:52:39 -0800

    manifest

---
+++ Homework4/serviceWorker.js
@@ -0,0 +1,28 @@
+const progressiveWebApplication = "PWA-v1"
+const assets = [
+  "/",
+  "./index.html",
+  "./style.css",
+  "./app.js",
+  "./Icon-192.png",
+  "./Icon-256.png",
+  "./Icon-384.png",
+  "./Icon-512-1.png",
+  "./lightbulue.jpg",
+  "./lightgold.jpg",
+]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(progressiveWebApplication).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })
