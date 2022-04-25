a3307fe207afd5f219de13a370078b0a65c1c2ca
andre <andrewsprague80@yahoo.com>
2022-04-25 15:56:56 -0800

    js

--- Homework4/app.js
+++ Homework4/app.js
@@ -1,3 +1,17 @@
+const container = document.querySelector(".container")
+const icons = [
+  { name: "icon192", image: "Icon-192.png" },
+  { name: "icon256", image: "Icon-256.png" },
+  { name: "icon384", image: "Icon-384.png" },
+  { name: "icon512", image: "Icon-512-1.png" },
+  { name: "lightbulueBG", image: "lightblue.jpg" },
+  { name: "lightgoldBG", image: "lightgold.jpg" },
+]
+
+
+
+
+
 if ("serviceWorker" in navigator) {
     window.addEventListener("load", function() {
       navigator.serviceWorker
