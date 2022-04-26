1854540e5a56e3c259835511ec14d7edc26672e6
andre <andrewsprague80@yahoo.com>
2022-04-25 17:35:48 -0800

    sw

--- pwahw/serviceWorker.js
+++ pwahw/serviceWorker.js
@@ -1,29 +1,69 @@
-const progressiveWebApplication = "PWA-v1"
-const assets = [
-  "/",
-  "/index.html",
-  "/styles.css",
-  "/app.js",
-  "/Icon-192.png",
-  "/Icon-256.png",
-  "/Icon-384.png",
-  "/Icon-512-1.png",
-  "/lightblue.jpg",
-  "/lightgold.jpg",
-]
+// const progressiveWebApplication = "PWA-v1"
+// const assets = [
+//   "/styles.css",
+//   "/app.js",
+// ]
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(progressiveWebApplication).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
-self.addEventListener("fetch", fetchEvent => {
-    fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-        return res || fetch(fetchEvent.request)
-      })
-    )
-  })
+// self.addEventListener("install", installEvent => {
+//   installEvent.waitUntil(
+//     caches.open(progressiveWebApplication).then(cache => {
+//       cache.addAll(assets)
+//     })
+//     )
+//   })
+//   self.addEventListener("fetch", fetchEvent => {
+//     fetchEvent.respondWith(
+//       caches.match(fetchEvent.request).then(res => {
+//         return res || fetch(fetchEvent.request)
+//       })
+//       )
+//     })
+    var CACHE_VERSION = 'myapp-v1';
+    var CACHE_FILES = [
+      "/",
+      "/index.html",
+      'app.js',
+      'styles.css',
+      "/Icon-192.png",
+      "/Icon-256.png",
+      "/Icon-384.png",
+      "/Icon-512-1.png",
+      "/lightblue.jpg",
+      "/lightgold.jpg",
+  ];
   
+  self.addEventListener('install', event => {
+      console.log('SW installed');
+      event.waitUntil(
+          caches
+          .open(CACHE_VERSION)
+          .then(cache => {
+              console.log('SW caching files');
+              cache.addAll(CACHE_FILES)
+          })
+          .then(() => self.skipWaiting())
+      );
+  });
+  
+  self.addEventListener('activate', event => {
+      console.log('SW activated');
+      event.waitUntil(
+          caches.keys().then(keyNames => {
+              return Promise.all(
+                  keyNames.map(key => {
+                      if(key !== CACHE_VERSION) {
+                          console.log('SW clearing old caches');
+                          return caches.delete(key);
+                      }
+                  })
+              );
+          })
+      );
+  });
+  
+  self.addEventListener('fetch', event => {
+      console.log('SW fetching');
+      event.respondWith(
+          fetch(event.request).catch(() => caches.match(event.request))
+      );
+  });  
